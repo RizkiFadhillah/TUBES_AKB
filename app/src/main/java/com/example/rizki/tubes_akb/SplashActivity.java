a8b84aa8b0008a8376623eeb28124f95d48de9c6
doniwiranata <bandungbahatam@gmail.com>
2019-08-13 23:25:20 +0700

    TUBES

---
+++ app/src/main/java/com/example/rizki/tubes_akb/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.rizki.tubes_akb;
+/*nama  : Mochamad Rizki Fadhillah
+  nim   : 10116017
+  Tanggal pengiriman : 12 - 08 - 2019
+  kelas : IF-1/AKB-1
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
